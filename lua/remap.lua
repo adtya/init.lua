bb3b18454abc03b8278106442bb1f268d9db82f3
Adithya Nair <adtya@adtya.xyz>
2024-02-10 23:58:35 +0530

    fix mapping

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<C-a>", vim.cmd.bprevious)
-vim.keymap.set("n", "<C-d>", vim.cmd.bNext)
+vim.keymap.set("n", "<C-d>", vim.cmd.bnext)
 vim.keymap.set("n", "<C-h>", vim.cmd.nohlsearch)
