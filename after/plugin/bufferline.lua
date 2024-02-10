7f9bd8a242f88ef562d0e7fec130352905009382
Adithya Nair <adtya@adtya.xyz>
2024-02-10 23:59:40 +0530

    setup bufferline

---
+++ after/plugin/bufferline.lua
@@ -0,0 +1,17 @@
+vim.opt.termguicolors = true
+require('bufferline').setup{
+  options = {
+    diagnostics = "nvim_lsp",
+    themable = false,
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "File Tree",
+        separator = true
+      }
+    },
+    separator_style = "slant",
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+  }
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,14 @@ return require('packer').startup(function(use)
   use 'Mofiqul/dracula.nvim'
 
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.5', requires = { {'nvim-lua/plenary.nvim'} } }
+  use {
+    'akinsho/bufferline.nvim',
+    as = "bufferline.nvim",
+    version = '4.5.0',
+    requires = {
+      {'nvim-tree/nvim-web-devicons'}
+    }
+  }
 
   use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
 
