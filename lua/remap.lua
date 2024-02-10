facc675de3b1aeada3a1fa98061eb68dff4e859d
Adithya Nair <adtya@adtya.xyz>
2024-02-10 23:58:44 +0530

    close buffers

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<C-a>", vim.cmd.bprevious)
 vim.keymap.set("n", "<C-d>", vim.cmd.bnext)
 vim.keymap.set("n", "<C-h>", vim.cmd.nohlsearch)
+vim.keymap.set("n", "<leader>w", "<cmd>bdelete<Return><cmd>bnext<Return>")
+
