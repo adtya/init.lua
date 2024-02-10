e1b1bed2c4e20610843b86546ed4d6c772b2d655
Adithya Nair <adtya@adtya.xyz>
2024-02-11 00:24:07 +0530

    install gitsigns

---
+++ after/plugin/gitsigns.lua
@@ -0,0 +1,9 @@
+require('gitsigns').setup{
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text_pos = 'right_align',
+    delay = 0
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>'
+}
+
