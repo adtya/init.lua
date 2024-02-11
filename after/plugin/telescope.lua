34b4192e326e08659291393924e11476c790d1ad
Adithya Nair <adtya@adtya.xyz>
2024-02-11 18:15:06 +0530

    update telescope bindingfs

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fa", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fp", builtin.live_grep, {})
