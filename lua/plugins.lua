7872058580272710fc0008849e88d8b998240c8f
Adithya Nair <adtya@adtya.xyz>
2024-02-11 12:53:08 +0530

    refactor

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,5 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'Mofiqul/dracula.nvim'
   use 'lewis6991/gitsigns.nvim'
   use "lukas-reineke/indent-blankline.nvim"
   use "mrcjkb/nvim-lastplace"
@@ -12,6 +11,13 @@ return require('packer').startup(function(use)
     requires = {
       {'nvim-tree/nvim-web-devicons'}
     }
+  }
+
+  use {
+    'Mofiqul/dracula.nvim',
+    config = function()
+      vim.cmd[[colorscheme dracula]]
+    end
   }
 
   use {
