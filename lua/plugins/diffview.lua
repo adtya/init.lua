1c969df226e1c3bf07a22a238e4a4b76de1e3bb2
Adithya Nair <adtya@adtya.xyz>
2024-06-10 01:40:16 +0530

    move diffview into misc plugins

--- lua/plugins/diffview.lua
+++
@@ -1,4 +0,0 @@
-local Plugin = { "sindrets/diffview.nvim" }
-Plugin.dependencies = { "nvim-tree/nvim-web-devicons" }
-
-return Plugin
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 local Plugins = {
   { "stevearc/dressing.nvim", opts = {}, event = "VeryLazy" },
+  { "sindrets/diffview.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
   { "nvim-tree/nvim-web-devicons", lazy = true },
 }
 
